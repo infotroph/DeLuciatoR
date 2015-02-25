37a669f73d7779dbd9247c838dc65deecef90838
Chris Black <chris@bodger.org>
2015-02-24 19:20:14 -0600

    shorter ticks, thickness relative to pointsize

--- ggthemes.r
+++ ggthemes.r
@@ -2,7 +2,9 @@ theme_ggEHD = function(base_size=18, ...){
 	(theme_bw(base_size=base_size) %+% theme(
 		panel.grid.major = element_blank(),
 		panel.grid.minor = element_blank(),
-		axis.ticks.length = unit(-(base_size*0.75), "points"),
+		rect = element_rect(size=rel(0.75)),
+		line = element_line(size=rel(1)),
+		axis.ticks.length = unit(-(base_size*0.5), "points"),
 		axis.ticks.margin = unit((base_size*1.5), "points"),
 		plot.margin = unit(base_size*c(1,1,1,1), "points"),
 		text=element_text( # Can we inherit some of these?
