3de7339d8ec37b8a517b674e3ccadc816744a67f
Chris Black <chris@bodger.org>
2015-02-24 19:17:31 -0600

    make sizes relative to configurable base_size

--- ggthemes.r
+++ ggthemes.r
@@ -1,14 +1,14 @@
-theme_ggEHD = function(...){
-	(theme_bw() %+% theme(
+theme_ggEHD = function(base_size=18, ...){
+	(theme_bw(base_size=base_size) %+% theme(
 		panel.grid.major = element_blank(),
 		panel.grid.minor = element_blank(),
-		axis.ticks.length = unit(-0.75, "lines"),
-		axis.ticks.margin = unit(1.5, "lines"),
-		plot.margin = unit(c(1,1,1,1), "lines"),
+		axis.ticks.length = unit(-(base_size*0.75), "points"),
+		axis.ticks.margin = unit((base_size*1.5), "points"),
+		plot.margin = unit(base_size*c(1,1,1,1), "points"),
 		text=element_text( # Can we inherit some of these?
 			family="",
 			face="plain",
-			size=18,
+			size=base_size,
 			hjust=0.5,
 			vjust=0.5,
 			angle=0),
