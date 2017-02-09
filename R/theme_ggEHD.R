c126018e7ec8dd75918ba016c7120b79f7296080
Kevin Wolz <wolz1@illinois.edu>
2017-02-09 09:04:49 -0600

    misc errors
    panel border color and axis text margins for new ggplot2 versions

--- R/theme_ggEHD.R
+++ R/theme_ggEHD.R
@@ -1,6 +1,7 @@
 theme_ggEHD = function(base_size=18, base_family=""){
 	thm = (theme_bw(base_size=base_size, base_family=base_family) %+% theme(
-		panel.border = element_rect(size=2),
+		panel.border = element_rect(size=2,
+					   color="black"),
 		panel.grid.major = element_blank(),
 		panel.grid.minor = element_blank(),
 		rect = element_rect(size=rel(0.75)),
@@ -8,7 +9,7 @@ theme_ggEHD = function(base_size=18, base_family=""){
 		#axis.line = element_line(size=rel(1)),
         	axis.line = element_blank(),
 		axis.ticks = element_line(size=rel(1),
-					 color = "black"),
+					 color="black"),
 		axis.ticks.length = unit(-(base_size*0.5), "points"),
 		plot.margin = unit(base_size*c(1,1,1,1), "points"),
 		text=element_text( # Can we inherit some of these?
@@ -20,9 +21,9 @@ theme_ggEHD = function(base_size=18, base_family=""){
 			vjust=0.5,
 			angle=0),
 		axis.text=element_text(size=rel(0.8), 
-				       color = "black"),
-		axis.text.x = element_text(margin = margin(t = (base_size*0.8), unit = "points")), 
-        	axis.text.y = element_text(margin = margin(r = (base_size*0.8), unit = "points")),
+				       color="black"),
+		axis.text.x = element_text(margin=margin(t=(base_size*0.8), unit="points")), 
+        	axis.text.y = element_text(margin=margin(r=(base_size*0.8), unit="points")),
 		axis.title.x=element_text(vjust=-1),
 		axis.title.y=element_text(vjust=2),
 		legend.key=element_blank(),
@@ -31,8 +32,8 @@ theme_ggEHD = function(base_size=18, base_family=""){
 
 	if(packageVersion("ggplot2") >= 2.0){
 		return(thm %+% theme(
-			axis.text.x = element_text(margin=margin(t=base_size*1.5, unit="points")),
-			axis.text.y = element_text(margin=margin(r=base_size*1.5, unit="points"))))
+			axis.text.x = element_text(margin=margin(t=base_size*0.8, unit="points")),
+			axis.text.y = element_text(margin=margin(r=base_size*0.8, unit="points"))))
 	}else{
 		return(thm %+% theme(
 			axis.ticks.margin = unit((base_size*1.5), "points")))
